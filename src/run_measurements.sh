474ac270471ed71d2f6615c8361643a881f04be2
Pedro Marcondes <pedromarcondes94@hotmail.com>
2017-07-07 22:26:43 -0300

    Update run_measurements.sh

--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 MEASUREMENTS=10
 ITERATIONS=1
-THREAD_IT=8
+THREAD_IT=2
 INITIAL_THREAD_NUM=1 
 INITIAL_SIZE=8192
 
@@ -18,10 +18,10 @@ mkdir results
 mkdir results/$THREAD_IT
 
 for ((i=1; i<=$ITERATIONS; i++)); do
-        perf stat -r $MEASUREMENTS mpirun -np $THREAD_IT -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
-        perf stat -r $MEASUREMENTS mpirun -np $THREAD_IT -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
-        perf stat -r $MEASUREMENTS mpirun -np $THREAD_IT -hostfile hostfile ./$NAME_SEQ --allow-run-as-root 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
-        perf stat -r $MEASUREMENTS mpirun -np $THREAD_IT -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+        perf stat -r $MEASUREMENTS mpirun -np 8 -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
+        perf stat -r $MEASUREMENTS mpirun -np 8 -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
+        perf stat -r $MEASUREMENTS mpirun -np 8 -hostfile hostfile ./$NAME_SEQ --allow-run-as-root 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
+        perf stat -r $MEASUREMENTS mpirun -np 8 -hostfile hostfile ./$NAME_SEQ --allow-run-as-root -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
         SIZE=$(($SIZE * 2))
 done
 
